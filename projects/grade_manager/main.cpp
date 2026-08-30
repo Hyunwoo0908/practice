2a147c11e09b0a6c75dfd23c2d08a53eb886db5a
Eden <zeratul125@naver.com>
2026-08-31 01:03:24 +0900

    성적(2단계) - 최고점, 최저점 함수 추가 및 출력 포맷 개선

--- projects/grade_manager/main.cpp
+++ projects/grade_manager/main.cpp
@@ -12,6 +12,27 @@ double calculateAverage(int scores[], int count){
 
     return (double)sum/count;
 }
+int findMax(int scores[], int count){
+    int max = scores[0];
+
+    for(int i = 0; i < count; i++){
+        if(max < scores[i]){
+            max = scores[i];
+        }
+    }
+    return max;
+}
+int findMin(int scores[], int count){
+    int min = scores[0];
+
+    for(int i = 0; i < count; i++){
+        if(min > scores[i]){
+            min = scores[i];
+        }
+    }
+    return min;
+
+}
 
 int main(){
     int student;
@@ -26,6 +47,15 @@ int main(){
 
     double avg = calculateAverage(arr, student);
     
-    cout<< avg;
+    cout<<"\n평균 값 :" << avg<<"\n";
+
+    int MaxValue = findMax(arr, student);
+
+    cout<<"최고점 :"<< MaxValue<<"\n";
+
+    int MinValue = findMin(arr, student);
+
+    cout<<"최저점 :"<<MinValue<<"\n";
+
     return 0;
 }
