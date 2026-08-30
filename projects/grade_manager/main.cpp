c77880c8eebd2a466e9452362d9a9e93c26bc9f0
Eden <zeratul125@naver.com>
2026-08-31 00:49:42 +0900

    성적(2단계) - 평균 계산 함수 추가

--- projects/grade_manager/main.cpp
+++ projects/grade_manager/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+double calculateAverage(int scores[], int count){
+
+    int sum = 0;
+
+    for(int i = 0; i < count; i++){
+
+        sum += scores[i];
+    }
+
+    return (double)sum/count;
+}
+
 int main(){
     int student;
     int arr[5];
@@ -12,6 +24,8 @@ int main(){
         cout<<arr[i]<<" ";
     }
 
-
+    double avg = calculateAverage(arr, student);
+    
+    cout<< avg;
     return 0;
 }
