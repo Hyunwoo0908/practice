b6b38ebdce33db06340870c1a57a71a123581396
Eden <zeratul125@naver.com>
2026-08-31 17:34:25 +0900

    성적관리 Java

---
+++ projects/grade_manager/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Main{
+
+    static double calculateAverage(int scores[], int count){
+        int sum = 0;
+        for(int i = 0; i < count; i++){
+            sum += scores[i];
+        }
+        return (double) sum / count;
+    }
+
+    static int findMax(int scores[], int count){
+        int max = scores[0];
+        for(int i = 0; i < count; i++){
+            if(max < scores[i]){
+                max = scores[i];
+            }
+        }
+        return max;
+    }
+
+    static int findMin(int scores[], int count){
+        int min = scores[0];
+        for(int i = 0; i < count; i++){
+            if(min > scores[i]){
+                min = scores[i];
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int student = sc.nextInt();
+        int[] arr = new int[5];
+
+        for(int i = 0; i < student; i++){
+            arr[i] = sc.nextInt();
+            System.out.print(arr[i] + " ");
+        }
+
+        double avg = calculateAverage(arr, student);
+        System.out.println("평균값: "+avg);
+
+        int MaxValue = findMax(arr, student);
+        System.out.println("최대값 : "+MaxValue);
+
+        int MinValue = findMin(arr, student);
+        System.out.println("최소값 : "+MinValue);
+
+        for(int i = 0; i < student; i++){
+            if(arr[i] > avg){
+                System.out.println(arr[i] + "점 - 평균 이상");
+            }else{
+                System.out.println(arr[i] + "점 - 평균 이하");
+            }
+        }
+    }
+
+}
